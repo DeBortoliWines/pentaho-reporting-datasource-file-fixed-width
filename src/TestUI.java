a86909ea6b4f5b4889f040103261a918d1d28b57
Pieter van der Merwe <pietercvdm@gmail.com>
2017-05-25 20:36:59 +1000

    Added test class to test the UI without having to build / deploy the plugin.

---
+++ src/TestUI.java
@@ -0,0 +1,79 @@
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+import javax.swing.JDialog;
+
+import org.apache.xmlbeans.impl.common.IdentityConstraint.FieldState;
+import org.codehaus.groovy.transform.FieldASTTransformation;
+import org.pentaho.reporting.engine.classic.extensions.datasources.filefixedwidth.FileFixedWidthConfiguration;
+import org.pentaho.reporting.engine.classic.extensions.datasources.filefixedwidth.FileFixedWidthConfiguration.Field;
+import org.pentaho.reporting.engine.classic.extensions.datasources.filefixedwidth.FileFixedWidthConfiguration.Record;
+import org.pentaho.reporting.ui.datasources.filefixedwidth.FileFixedWidthPanel;
+
+public class TestUI {
+
+  public static void main(String[] args) {
+    FileFixedWidthConfiguration config = new FileFixedWidthConfiguration();
+    config.setFileLocation("/home/pietercvdm/Documents/Werk/Invoice/invoice.txt");
+    
+    ArrayList<Record> records = new ArrayList<Record>();
+    
+    Record rec = config.new Record();
+    rec.setDescription("Header");
+    rec.setIdentifier("0");
+    
+    ArrayList<Field> fields = new ArrayList<Field>();
+    Field fld = config.new Field();
+    fld.setFieldName("invoice_number");
+    fld.setFieldType("String");
+    fld.setStart(1);
+    fld.setEnd(8);
+    fields.add(fld);
+    
+    fld = config.new Field();
+    fld.setFieldName("company_name");
+    fld.setFieldType("String");
+    fld.setStart(9);
+    fld.setEnd(47);
+    fields.add(fld);    
+    
+    Field[] fldArr = new Field[fields.size()];
+    fields.toArray(fldArr);
+    rec.setFields(fldArr);
+    records.add(rec);
+    
+    rec = config.new Record();
+    rec.setDescription("Detail");
+    rec.setIdentifier("2");
+    
+    fields = new ArrayList<>();
+    fld = config.new Field();
+    fld.setFieldName("customer_name");
+    fld.setFieldType("String");
+    fld.setStart(2);
+    fld.setEnd(39);
+    fields.add(fld);
+    
+    fldArr = new Field[fields.size()];
+    fields.toArray(fldArr);
+    rec.setFields(fldArr);
+    records.add(rec);
+    
+    Record [] rcs = new Record[records.size()];
+    records.toArray(rcs);
+    config.setRecords(rcs);
+    
+    // Open the dialog to configure your data source
+    JDialog dialog = new JDialog();
+    dialog.getContentPane().setLayout(new BorderLayout());
+    FileFixedWidthPanel panel = new FileFixedWidthPanel();
+    panel.setQueryName("Queue1");
+    panel.setConfiguration(config);
+    panel.setVisible(true);
+    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+    dialog.getContentPane().add(panel,BorderLayout.CENTER);
+    dialog.setBounds(100, 100, 1024, 768);
+    dialog.setModal(true);
+    dialog.setVisible(true);
+  }
+}
